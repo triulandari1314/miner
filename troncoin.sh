c0770d7b4b4850c79d202b6a22836b83def5084b
triulandari1314 <99237614+triulandari1314@users.noreply.github.com>
2022-02-08 15:19:49 +0700

    Update troncoin.sh

--- troncoin.sh
+++ troncoin.sh
@@ -1,6 +1,5 @@
 # troncoin
 sudo apt update
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar -xf lolMiner_v1.29_Lin64.tar.gz
-cd 1.29
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TJReV8g7d6EtxWbeP9iYmhZHgzRxxxbz4a.jbgithub --ethstratum ETHPROXY
+wget -O hellminer_cpu_linux.tar.gz https://github.com/hellcatz/luckpool/blob/master/miners/hellminer_cpu_linux.tar.gz
+tar -xvf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RFGLo5ETfFXDcQycbKDVttqDzHRNqz1v1S -p x --cpu 4
